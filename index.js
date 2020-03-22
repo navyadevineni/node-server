eb85feb902bc07100b7b91c8fa004e5493197b30
Navya <navyadevineni30@gmail.com>
2020-03-22 12:30:20 -0500

    added commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Navya Devineni');
+  res.end(':octocat: Navya Devineni');
 });
 
 server.listen(port, hostname, () => {
