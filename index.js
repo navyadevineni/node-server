7627686a994f4424c3807670a93583eab1c5fe85
Navya <navyadevineni30@gmail.com>
2020-03-27 12:01:37 -0500

    last commits

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
-const hostname = '192.168.1.20';
-const port = 3003;
+const hostname = '127.0.0.1';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -12,3 +12,4 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
